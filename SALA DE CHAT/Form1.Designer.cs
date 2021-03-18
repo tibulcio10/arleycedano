39d1079fceea0284844e86be0dc927c82e820a57
tibulcio10 <60802422+tibulcio10@users.noreply.github.com>
2021-03-17 21:59:19 -0400

    update commit

---
+++ SALA DE CHAT/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace SALA_DE_CHAT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.lblUsurio = new System.Windows.Forms.Label();
+            this.txtMensaje = new System.Windows.Forms.TextBox();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnConectar.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConectar.Location = new System.Drawing.Point(202, 176);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(74, 32);
+            this.btnConectar.TabIndex = 1;
+            this.btnConectar.Text = "Ingresar ";
+            this.btnConectar.UseVisualStyleBackColor = false;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(158, 150);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(163, 20);
+            this.txtUsuario.TabIndex = 2;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnEnviar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnviar.Location = new System.Drawing.Point(327, 266);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(73, 36);
+            this.btnEnviar.TabIndex = 3;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = false;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // lblUsurio
+            // 
+            this.lblUsurio.AutoSize = true;
+            this.lblUsurio.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsurio.ForeColor = System.Drawing.Color.White;
+            this.lblUsurio.Location = new System.Drawing.Point(202, 125);
+            this.lblUsurio.Name = "lblUsurio";
+            this.lblUsurio.Size = new System.Drawing.Size(74, 22);
+            this.lblUsurio.TabIndex = 4;
+            this.lblUsurio.Text = "Usuario";
+            this.lblUsurio.Click += new System.EventHandler(this.lblUsurio_Click);
+            // 
+            // txtMensaje
+            // 
+            this.txtMensaje.Location = new System.Drawing.Point(77, 266);
+            this.txtMensaje.Name = "txtMensaje";
+            this.txtMensaje.Size = new System.Drawing.Size(200, 20);
+            this.txtMensaje.TabIndex = 5;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Arial", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.White;
+            this.lblTitulo.Location = new System.Drawing.Point(152, 12);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(176, 33);
+            this.lblTitulo.TabIndex = 6;
+            this.lblTitulo.Text = "Sala de chat";
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(77, 1);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(323, 225);
+            this.listBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(509, 348);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.txtMensaje);
+            this.Controls.Add(this.lblUsurio);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Sala de chat";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Label lblUsurio;
+        private System.Windows.Forms.TextBox txtMensaje;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ SALA DE CHAT/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Threading;
+using System.Net.Sockets;
+using System.IO;
+using Transitions;
+
+namespace SALA_DE_CHAT
+{
+    public partial class Form1 : Form
+    {
+        static private NetworkStream stream;
+        static private StreamWriter streamW;
+        static private StreamReader streamR;
+        static private TcpClient client = new TcpClient();
+        static private string David = "unknown";
+
+        private delegate void DaddItem(string s);
+
+        private void AddItem(string s)
+        {
+            listBox1.Items.Add(s);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void Listen()
+        {
+            while (client.Connected)
+            {
+                try
+                {
+                    this.Invoke(new DaddItem(AddItem), streamR.ReadLine());
+                }
+                catch
+                {
+                    MessageBox.Show("No se ha podido conectar al servidor");
+                    Application.Exit();
+                }
+            }
+        }
+
+        void Conectar()
+        {
+            try
+            {
+                client.Connect("127.0.0.1", 8000);
+                if (client.Connected)
+                {
+                    Thread t = new Thread(Listen);
+
+                    stream = client.GetStream();
+                    streamW = new StreamWriter(stream);
+                    streamR = new StreamReader(stream);
+
+                    streamW.WriteLine(David);
+                    streamW.Flush();
+
+                    t.Start();
+                }
+                else
+                {
+                    MessageBox.Show("Servidor no Disponible");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Servidor no Disponible");
+                Application.Exit();
+            }
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            David = txtUsuario.Text;
+            Conectar();
+
+            Transition t = new Transition(new TransitionType_EaseInEaseOut(900));
+            t.add(lblTitulo, "Left", 555);
+            t.add(txtUsuario, "Left", 555);
+            t.add(btnConectar, "Left", 555);
+            t.add(listBox1, "Left", 26);
+            t.add(txtMensaje, "Left", 26);
+            t.add(btnEnviar, "Left", 283);
+            t.add(lblUsurio, "Left", 555);
+            t.run();
+
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            streamW.WriteLine(txtMensaje.Text);
+            streamW.Flush();
+            txtMensaje.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnEnviar.Location = new Point(-239, 250);
+            txtMensaje.Location = new Point(-239, 250);
+            listBox1.Location = new Point(-329, 23);
+        }
+
+        private void lblUsurio_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
